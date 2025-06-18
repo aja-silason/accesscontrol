127df7786ec0ac57eae34fc457fee73cc4fee770
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-06-18 04:44:01 +0100

    add avoid view

--- src/app/view/index.tsx
+++ src/app/view/index.tsx
@@ -15,30 +15,31 @@ import axios from "axios";
 import { API_URL } from "@/app/viewmodel/utils/server/enpoint";
 import { routing } from "@/app/viewmodel/services/Navegation";
 import { Loading } from "@/app/view/components/loading";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 
 export default function LoginScreen() {
   const [link, setLink] = useState<string>("");
-  const [email, setEmail] =useState('');
-  const [password, setPassword] =useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [textBtn, setTextBtn] = useState("Entrar");
 
-  const {user, login, isLoading} = useAuth();
+  const { user, login, isLoading } = useAuth();
 
-  useEffect(()=> {
+  useEffect(() => {
 
-    if(!user){
-      null  
+    if (!user) {
+      null
     }
-    if(user){
-      if(user?.login?.role?.role == "A") {
-          routing.handleRouteHomeNonBackAgain();
-        } else if(user?.login?.role?.role == "B") {
-          routing.handleRouterHomeNoBack();
-        } else if(user == null){
-          null
-        }
-        return
+    if (user) {
+      if (user?.login?.role?.role == "A") {
+        routing.handleRouteHomeNonBackAgain();
+      } else if (user?.login?.role?.role == "B") {
+        routing.handleRouterHomeNoBack();
+      } else if (user == null) {
+        null
+      }
+      return
     }
   }, [user])
 
@@ -46,42 +47,54 @@ export default function LoginScreen() {
   const handleClick = async () => {
     await login(email, password);
   }
-  
-  
+
+
 
   return (
 
     <View style={Style.container}>
-      
-      <Header/>
 
-      <ScrollView style={{backgroundColor: "#fff", width: "100%"}} showsVerticalScrollIndicator={false}>
-      <View style={Style.loginData}>
-        
-        <View>
-          <Text style={Style.texth1}>Acessar conta</Text>
-          <Text style={Style.textp}>Por favor! Preencha o formulário com suas credenciais.</Text>
-        </View>
+      <Header />
 
-        <View style={Style.containerInput} >
-          <InputContainer>
-            <InputContainer.Field>E-mail</InputContainer.Field>
-            <InputContainer.Input place="seu@email.com" onChangeText={(email: any) => setEmail(email)} value={email}/>
-          </InputContainer>
-          <InputContainer>
-            <InputContainer.Field>Palavra passe</InputContainer.Field>
-            <InputContainer.Input place="********" secureText={true} onChangeText={(password: any) => setPassword(password)} value={password}/>
-          </InputContainer>
-          {
-          isLoading?  <Button text={<Loading/>} onClick={handleClick} disabled={isLoading}/>:   <Button text={textBtn} onClick={handleClick} disabled={isLoading}/>
-          }
-          
-          <View style={Style.madein}>
-            <MadeInAngolaSVG/>
+      <ScrollView style={{ backgroundColor: "#fff", width: "100%" }} showsVerticalScrollIndicator={false}>
+        <View style={Style.loginData}>
+
+          <View>
+            <Text style={Style.texth1}>Acessar conta</Text>
+            <Text style={Style.textp}>Por favor! Preencha o formulário com suas credenciais.</Text>
           </View>
-        </View>
 
-      </View>
+          <KeyboardAwareScrollView
+            style={{ flex: 1, width: "100%" }}
+            contentContainerStyle={{ flexGrow: 1, paddingBottom: 10 }}
+            enableOnAndroid
+            keyboardShouldPersistTaps="handled"
+            extraScrollHeight={20}
+            showsVerticalScrollIndicator={false}
+          >
+
+            <View style={Style.containerInput} >
+              <InputContainer style={{width: "100%"}}>
+                <InputContainer.Field>E-mail</InputContainer.Field>
+                <InputContainer.Input place="seu@email.com" onChangeText={(email: any) => setEmail(email)} value={email} />
+              </InputContainer>
+
+              <InputContainer>
+                <InputContainer.Field>Palavra passe</InputContainer.Field>
+                <InputContainer.Input place="********" secureText={true} onChangeText={(password: any) => setPassword(password)} value={password} />
+              </InputContainer>
+              {
+                isLoading ? <Button text={<Loading />} onClick={handleClick} disabled={isLoading} /> : <Button text={textBtn} onClick={handleClick} disabled={isLoading} />
+              }
+
+              <View style={Style.madein}>
+                {/*<MadeInAngolaSVG />*/}
+              </View>
+            </View>
+
+          </KeyboardAwareScrollView>
+
+        </View>
       </ScrollView>
     </View>
   );
@@ -91,31 +104,31 @@ export default function LoginScreen() {
 
 const Style = StyleSheet.create({
   container: {
-      flex: 1,
-      alignItems: "center",
-      backgroundColor: "rgba(255, 255, 255, 0.5)",
-      height: "100%",
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    height: "100%",
   },
   loginData: {
-      marginVertical: "10%",
-      gap: 30,
-      alignItems: "center",
-      width: "100%",
-      paddingHorizontal: 15,
+    marginVertical: "10%",
+    gap: 30,
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 15,
   },
   texth1: {
-      fontSize: 26,
-      fontWeight: "500",
-      color: Colors.black[200]
+    fontSize: 26,
+    fontWeight: "500",
+    color: Colors.black[200]
   },
   textp: {
-      color: Colors.black[300],
-      fontSize: 12
+    color: Colors.black[300],
+    fontSize: 12
   },
 
   containerInput: {
-      gap: 15,
-      width: "100%"
+    gap: 15,
+    width: "100%"
   },
   madein: {
     alignItems: "center"
