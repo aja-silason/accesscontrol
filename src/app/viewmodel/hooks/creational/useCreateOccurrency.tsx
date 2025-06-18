1b9f010c10cc99b94700430081675f6f7c5bcdd5
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-06-18 06:52:40 +0100

    feat: add occurrencyy

--- src/app/viewmodel/hooks/creational/useCreateOccurrency.tsx
+++ src/app/viewmodel/hooks/creational/useCreateOccurrency.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { useGetDatas } from "../useGetDatas";
 import { useAuth } from "../../context/AuthContext";
-import { Alert } from "react-native";
+import { Alert, Platform } from "react-native";
 import { API_URL } from "../../utils/server/enpoint";
 import axios from "axios";
 
 type typePayload = {
     distribuitorId?: string,
-    supervisorCode: string,
+    supervisorId: string,
     plataformId: string,
     driverCode: string,
     transportCode: string,
@@ -24,7 +24,7 @@ export const useCreateOccurrency = () => {
     const [searchVehicle, setSearchVehicle] = useState("");
     const [ocorrence, setOcorrence] = useState("");
 
-    const [datas, setDatas] = useState<typePayload>({driverCode: "", ocurrence: "", plataformId: "", transportCode: "", supervisorCode: ""});
+    const [datas, setDatas] = useState<typePayload>({ driverCode: "", ocurrence: "", plataformId: "", transportCode: "", supervisorId: "" });
 
     const { data: option } = useGetDatas("distribuitor");
     const { data: driver } = useGetDatas("driver");
@@ -41,7 +41,7 @@ export const useCreateOccurrency = () => {
     const vehicledata: any = vehicle?.filter((data: any) => data?.authorizationCode?.toLowerCase()?.includes(datas?.transportCode.toLocaleLowerCase()))
 
     const handleChange = (name: string, value: string) => {
-        setDatas((prevState: any) => ({...prevState, [name]: value}))
+        setDatas((prevState: any) => ({ ...prevState, [name]: value }))
     }
 
     const handleSubmit = async () => {
@@ -50,7 +50,7 @@ export const useCreateOccurrency = () => {
 
             const payload = {
                 distribuitorId: distribuitor,
-                supervisorCode: user?.login?.id,
+                supervisorId: user?.login?.id,
                 plataformId: user?.login?.plataform?.id,
                 driverCode: driverdata[0]?.id,
                 transportCode: vehicledata[0]?.id,
@@ -58,9 +58,9 @@ export const useCreateOccurrency = () => {
 
             }
 
-                console.log(JSON.stringify(payload, null, 2))
+            console.log(JSON.stringify(payload, null, 2))
 
-            const isValidate: Array<keyof typePayload> = ["distribuitorId", "driverCode", "ocurrence", "plataformId", "supervisorCode", "transportCode"];
+            const isValidate: Array<keyof typePayload> = ["distribuitorId", "driverCode", "ocurrence", "plataformId", "supervisorId", "transportCode"];
             for (const key of isValidate) {
                 if (payload[key] == undefined) {
                     setIsloading(false);
@@ -68,17 +68,50 @@ export const useCreateOccurrency = () => {
                 }
             }
 
-            await axios.post(`${API_URL}ocurrences`, payload, {
+            await axios.post(`${API_URL}occurences`, payload, {
                 headers: {
                     Authorization: `Bearer ${user?.authorizationToken}`
                 }
             })
 
-            
+
             setIsloading(true)
+            setDatas({ driverCode: "", ocurrence: "", plataformId: "", transportCode: "", supervisorId: "" });
 
         } catch (error) {
-            console.log("Alerta de erro na app", error);
+
+            if (axios.isAxiosError(error)) {
+                if (error.response) {
+                    const message = error?.response?.data?.error || "Erro desconhecido no servidor";
+                    console.log("Erro desconhecido:", error?.code || error?.response?.data?.error?.includes("Bad Request"));
+                    if (error?.code?.includes("ERR_BAD_REQUEST")) {
+                        if (Platform.OS == "web") alert("Alguma coisa correu mal, estamos resolvendo por você");
+                        Alert?.alert("Aviso", "Alguma coisa correu mal, estamos resolvendo por você");
+                        //return
+                    } else {
+                        //Alert.alert("Erro", message);
+                        if (Platform.OS == "web") alert("Alguma coisa correu mal, estamos resolvendo por você");
+                        Alert.alert("Aviso", "Alguma coisa correu mal, estamos resolvendo por você");
+                    }
+                } else if (error?.status == 201) {
+                    console.log('Sem resposta do servidor (Axios):', error.request);
+                    if (Platform.OS == "web") alert("Aplicado com sucesso");
+                    Alert.alert("Sucesso", "Aplicado com sucesso");
+                    return;
+                } else if (error.request) {
+                    console.log('Sem resposta do servidor (Axios):', error.request);
+                    if (Platform.OS == "web") alert("Verifique a sua conexão com a internet");
+                    Alert.alert("Aviso", "Verifique a sua conexão com a internet");
+                }
+            } else if (error instanceof TypeError) {
+                console.log('Erro de rede (fetch):', error.message);
+                if (Platform.OS == "web") alert("Erro de rede ou resposta inválida");
+                Alert.alert("Aviso", "Erro de rede ou resposta inválida");
+            } else {
+                console.log('Aviso desconhecido:', error);
+                if (Platform.OS == "web") alert("Alguma coisa correu mal, estamos resolvendo por você");
+                Alert.alert("Aviso", "Alguma coisa correu mal, estamos resolvendo por você");
+            }
         } finally {
             setIsloading(false);
         }
